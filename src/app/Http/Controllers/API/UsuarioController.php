33fbb5dfd29ef7a1a84eda04fcee4dd956f8601a
= <=>
2020-12-10 23:15:56 -0500

    API REST - Listar usuarios

---
+++ src/app/Http/Controllers/API/UsuarioController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use App\Http\Resources\UsuarioCollection;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //return Usuario::all();
+        return UsuarioCollection::collection(Usuario::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Usuario $usuario)
+    {
+        return new UsuarioCollection(Usuario::findOrFail($usuario->id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Usuario  $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Usuario $usuario)
+    {
+        //
+    }
+}
