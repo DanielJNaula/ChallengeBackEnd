3c10ab0a7f74f0714b95b0951d7c047483cb91a6
= <=>
2020-12-10 22:11:31 -0500

    Implementacion del modelo y seeder Usuario

--- src/app/Models/Usuario.php
+++ src/app/Models/Usuario.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+
+    protected $table = 'usuarios';
+
+    protected $fillable = [
+        'nombre',
+        'direccion',
+        'descripcion',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
--- src/database/seeders/UsuarioSeeder.php
+++ src/database/seeders/UsuarioSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Usuario;
+use DB;
 
 class UsuarioSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class UsuarioSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('usuarios')->delete(); 
+        // Registros a la tabla usuario
+        Usuario::create(array('nombre' => 'Gerardo Valverde', 'direccion'=>'Sangolqui - San Sebastian', 'descripcion'=>'Doctor, apasionado por el cuidado de ancian@s'));
+        
+        Usuario::create(array('nombre' => 'Paulina Moreno', 'direccion'=>'Batan - Canal 8', 'descripcion'=>'Veterinaria, apasionado por el cuidado de animales'));
+
+        Usuario::create(array('nombre' => 'Daniel Naula', 'direccion'=>'Chillogallo - Buenaventura', 'descripcion'=>'Ing. en sistemas, apasionado por la programacion'));
     }
 }
